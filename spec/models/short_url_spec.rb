43b922f5a02a8de272b1c1fe7a4c5f30c25f47ca
Jibran Kalia <jibran.kalia@gmail.com>
2020-02-22 15:59:30 -0600

    Require destination to be RFC2396 compliant

--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ShortUrl, type: :model do
   describe "create" do 
     it "is successful when slug and destination is present" do
       expect { 
-        ShortUrl.create!(slug: "1234", destination: "www.google.com") 
+        ShortUrl.create!(slug: "1234", destination: "http://www.google.com") 
       }.to change { ShortUrl.count }.by(1)
     end
 
@@ -16,7 +16,13 @@ RSpec.describe ShortUrl, type: :model do
 
     it "fails when slug is missing" do 
       expect { 
-        ShortUrl.create!(destination: "www.google.com")
+        ShortUrl.create!(destination: "http://www.google.com")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'fails if destination is not a valid RFC2396 url' do
+      expect { 
+        ShortUrl.create!(slug: "1234", destination: "google.com") 
       }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
