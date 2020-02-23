50f617badc1bd0c1f58ce8394c258313d4737466
Jibran Kalia <jibran.kalia@gmail.com>
2020-02-23 08:48:33 -0600

    Renamed route to redirect

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe ShortUrlsController, type: :controller do
     it "redirects to the destination if short url is present" do
       destination = "http://www.google.com"
       short_url = ShortUrl.create!(slug: "1234", destination: destination) 
-      get :show, params: { id: '1234' }
+      get :redirect, params: { id: '1234' }
       assert_redirected_to destination
     end
 
     it 'shows 404 page if short url is not present' do
-      get :show, params: { id: 'abcd' }
+      get :redirect, params: { id: 'abcd' }
       assert_response :missing
       assert_template 'error_pages/404'
     end
