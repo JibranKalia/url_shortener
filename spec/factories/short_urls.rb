733c588ac07550764836688c349b124cad8bf354
Jibran Kalia <jibran.kalia@gmail.com>
2020-02-23 13:02:19 -0600

    Annotating

--- spec/factories/short_urls.rb
+++ spec/factories/short_urls.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: short_urls
+#
+#  id          :bigint           not null, primary key
+#  destination :string           not null
+#  slug        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_short_urls_on_destination  (destination) UNIQUE
+#  index_short_urls_on_slug         (slug) UNIQUE
+#
 FactoryBot.define do
   factory :short_url do
     sequence(:slug) { |n| n.to_s }
--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: short_urls
+#
+#  id          :bigint           not null, primary key
+#  destination :string           not null
+#  slug        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_short_urls_on_destination  (destination) UNIQUE
+#  index_short_urls_on_slug         (slug) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe ShortUrl, type: :model do
